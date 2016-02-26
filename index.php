c1b448855f10e21a6193d8338f12ee2146956931
Dimon <DimonVelikii-1992@mail.ru>
2016-02-26 20:54:21 +0500

    first commit

---
+++ index.php
@@ -0,0 +1,118 @@
+<?php
+	abstract class Duck
+	{
+		public function swim()
+		{
+			echo 'swim';
+		}
+
+		abstract public function display();
+	}
+
+	interface Flyable
+	{
+		public function fly();
+	}
+
+	interface Quackable
+	{
+		public function quack();
+	}
+
+	class MallardDuck extends Duck implements Flyable, Quackable
+	{
+		public function fly()
+		{
+			echo 'fly';
+		}
+
+		public function quack()
+		{
+			echo 'quack';
+		}
+
+		public function display()
+		{
+			echo 'Mallard Duck';
+		}
+	}
+
+	class RedheadDuck extends Duck implements Flyable, Quackable
+	{
+		public function fly()
+		{
+			echo 'fly';
+		}
+
+		public function quack()
+		{
+			echo 'quack';
+		}
+
+		public function display()
+		{
+			echo 'RedheadDuck';
+		}
+	}
+
+	class RubberDuck extends Duck implements Quackable
+	{
+		public function quack()
+		{
+			echo 'quack';
+		}
+
+		public function display()
+		{
+			echo 'RubberDuck';
+		}
+	}
+
+	class DecoyDuck extends Duck
+	{
+		public function display()
+		{
+			echo 'Decoy Duck';
+		}
+	}
+//================
+	interface FlyBehavior
+	{
+		public function fly();
+	}
+	class FlyWithWings implements FlyBehavior
+	{
+		public function fly()
+		{
+			echo 'fly';
+		}
+	}
+	class FlyNoWay implements FlyBehavior
+	{
+		public function fly(){}
+	}
+
+	interface QuackBehavior
+	{
+		public function quack();
+	}
+	class Quack implements QuackBehavior
+	{
+		public function quack()
+		{
+			echo 'quack';
+		}
+	}
+	class Squack implements QuackBehavior
+	{
+		public function quack()
+		{
+			echo 'squack';
+		}
+	}
+	class MuteQuack implements QuackBehavior
+	{
+		public function quack(){}
+	}
+
+?>
---
+++ news.php
@@ -0,0 +1,130 @@
+<?php
+
+abstract class Duck
+{
+    //type interface
+    public $fly;
+    public $quack;
+
+    public function setFly(Fly $fly)
+    {
+        $this->fly = $fly;
+    }
+    public function setQuack(Quack $quack)
+    {
+        $this->quack = $quack;
+    }
+    //required function
+    public abstract function display();
+
+    //implements fly()
+    public function performFly()
+    {
+        $this->fly->fly();
+    }
+    //implement quack()
+    public function performQuack()
+    {
+        $this->quack->quack();
+    }
+}
+
+interface Fly
+{
+    public function fly();
+}
+class FlyYes implements Fly
+{
+    public function fly()
+    {
+        echo 'fly';
+    }
+}
+class FlyNo implements Fly
+{
+    public function fly()
+    {
+        echo 'no fly';
+    }
+}
+
+interface Quack
+{
+    public function quack();
+}
+class QuackYes implements Quack
+{
+    public function quack()
+    {
+        echo 'quack';
+    }
+}
+class QuackNo implements Quack
+{
+    public function quack()
+    {
+        echo 'no quack';
+    }
+}
+
+//MalladDuck
+class MallardDuck extends Duck
+{
+    public function __construct(Fly $fly, Quack $quack)
+    {
+        $this->fly = $fly;
+        $this->quack = $quack;
+    }
+
+    public function display()
+    {
+        echo 'mallard duck';
+    }
+}
+
+class RubberDuck extends Duck
+{
+    public function __construct(Fly $fly, Quack $quack)
+    {
+        $this->fly = $fly;
+        $this->quack = $quack;
+    }
+
+    public function display()
+    {
+        echo 'rubber duck';
+    }
+}
+
+//object MallardDuck
+$mallard_duck = new MallardDuck(new FlyNo(), new QuackNo());
+$mallard_duck->performFly();
+echo '<br>';
+$mallard_duck->performQuack();
+echo '<br>';
+$mallard_duck->setFly(new FlyYes());
+$mallard_duck->performFly();
+echo '<br>';
+$mallard_duck->setQuack(new QuackYes());
+$mallard_duck->performQuack();
+echo '<br>';
+$mallard_duck->display();
+echo '<br>';
+echo '<br>';
+
+//object RubberDuck
+$rubber_duck = new RubberDuck(new FlyYes(), new QuackYes());
+$rubber_duck->performFly();
+echo '<br>';
+$rubber_duck->performQuack();
+echo '<br>';
+$rubber_duck->setFly(new FlyNo());
+$rubber_duck->performFly();
+echo '<br>';
+$rubber_duck->setQuack(new QuackNo());
+$rubber_duck->performQuack();
+echo '<br>';
+$rubber_duck->display();
+echo '<br>';
+
+?>
